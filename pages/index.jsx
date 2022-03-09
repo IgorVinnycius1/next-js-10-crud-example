571690db361be243cd136b51336bd85c75adc900
IgorVinnycius1 <igor.vinnycius1@gmail.com>
2022-03-09 20:39:03 -0300

    Algumas mods

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,73 @@
+import { useState, useEffect } from 'react';
+
 import { Link } from 'components';
+import { userService } from 'services';
 
-export default Home;
+export default Index;
 
-function Home() {
+function Index() {
+    const [users, setUsers] = useState(null);
+
+    useEffect(() => {
+        userService.getAll().then(x => setUsers(x));
+    }, []);
+
+    function deleteUser(id) {
+        setUsers(users.map(x => {
+            if (x.id === id) { x.isDeleting = true; }
+            return x;
+        }));
+        userService.delete(id).then(() => {
+            setUsers(users => users.filter(x => x.id !== id));
+        });
+    }
+
     return (
         <div>
-            <h1>Next.js 10 - CRUD Example with React Hook Form</h1>
-            <p>An example app showing how to list, add, edit and delete user records with Next.js 10 and the React Hook Form library.</p>
-            <p><Link href="/users">&gt;&gt; Manage Users</Link></p>
+            <h1>Empresas</h1>
+            <Link href="/users/add" className="btn btn-sm btn-success mb-2">Add User</Link>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th style={{ width: '30%' }}>Name</th>
+                        <th style={{ width: '30%' }}>Email</th>
+                        <th style={{ width: '30%' }}>Role</th>
+                        <th style={{ width: '10%' }}></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users && users.map(user =>
+                        <tr key={user.id}>
+                            <td>{user.title} {user.firstName} {user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td>{user.role}</td>
+                            <td style={{ whiteSpace: 'nowrap' }}>
+                                <Link href={`/users/edit/${user.id}`} className="btn btn-sm btn-primary mr-1">Edit</Link>
+                                <button onClick={() => deleteUser(user.id)} className="btn btn-sm btn-danger btn-delete-user" disabled={user.isDeleting}>
+                                    {user.isDeleting 
+                                        ? <span className="spinner-border spinner-border-sm"></span>
+                                        : <span>Delete</span>
+                                    }
+                                </button>
+                            </td>
+                        </tr>
+                    )}
+                    {!users &&
+                        <tr>
+                            <td colSpan="4" className="text-center">
+                                <div className="spinner-border spinner-border-lg align-center"></div>
+                            </td>
+                        </tr>
+                    }
+                    {users && !users.length &&
+                        <tr>
+                            <td colSpan="4" className="text-center">
+                                <div className="p-2">No Users To Display</div>
+                            </td>
+                        </tr>
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
